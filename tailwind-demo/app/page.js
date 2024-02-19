54b3666a578b9c00903014b4d80f6d96097a7258
shaunajisc <100135505+shaunajisc@users.noreply.github.com>
2024-02-19 16:39:34 +0000

    plain html for card

--- tailwind-demo/app/page.js
+++ tailwind-demo/app/page.js
@@ -2,8 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex lg:bg-blue-500 bg-pink-900 font-mono text-2xl h-screen justify-center items-center w-full ">
-      <p className="hover:text-sm hover:animate-spin">HELLO WORLD</p>
-    </main>
+    <div>
+      <img src="/card-top.jpg" alt="Sunset in the mountains" />
+      <div>
+        <div>The Coldest Sunset</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div>
+        <span>#photography</span>
+        <span>#travel</span>
+        <span>#winter</span>
+      </div>
+    </div>
   );
 }
