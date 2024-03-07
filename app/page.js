c6b125296636551e973d9b4ff3bc5f2b27ec2032
shaunajisc <100135505+shaunajisc@users.noreply.github.com>
2024-03-07 11:19:33 +0000

    tailwind styles applied

--- app/page.js
+++ app/page.js
@@ -2,20 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <img src="/card-top.jpg" alt="Sunset in the mountains" />
-      <div>
-        <div>The Coldest Sunset</div>
-        <p>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg m-12 md:max-w-lg">
+      <img class="w-full" src="/card-top.jpg" alt="Sunset in the mountains" />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p class="text-gray-700 text-base">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
           quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
           nihil.
         </p>
       </div>
-      <div>
-        <span>#photography</span>
-        <span>#travel</span>
-        <span>#winter</span>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block hover:bg-cypress-blue hover:text-gray-100 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #photography
+        </span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #travel
+        </span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #winter
+        </span>
       </div>
     </div>
   );
